466649987a6548cd448e5d80576b60ea438b6113
Rafael Marques <rafaelmarques.rm86@gmail.com>
2022-10-27 13:10:40 -0300

    Recapitulação

---
+++ Recapitulacao exec orient obj 1/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recapitulacao_exec_orient_obj_1
+{
+    internal class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+    }
+}
---
+++ Recapitulacao exec orient obj 1/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais 
+velha. */
+
+using Recapitulacao_exec_orient_obj_1;
+
+Pessoa p1, p2;
+
+p1 = new Pessoa();
+p2 = new Pessoa();
+
+Console.WriteLine("Informe o nome da primeira pessoa:");
+p1.Nome = Console.ReadLine();
+Console.WriteLine("Informe a idade:");
+p1.Idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe o nome da segunda pessoa:");
+p2.Nome = Console.ReadLine();
+Console.WriteLine("Informe a idade:");
+p2.Idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Dados da primeira pessoa:");
+Console.WriteLine("Nome: " + p1.Nome);
+Console.WriteLine("Idade: " + p1.Idade);
+
+Console.WriteLine("Dados da segunda pessoa:");
+Console.WriteLine("Nome: " + p2.Nome);
+Console.WriteLine("Idade: " + p2.Idade);
+
+if(p1.Idade > p2.Idade)
+{
+    Console.WriteLine("Pessoa mais velha: " + p1.Nome);
+}
+else
+{
+    Console.WriteLine("Pessoa mais velha: " + p2.Nome);
+}
